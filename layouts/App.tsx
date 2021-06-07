6465923823cda223666f2fa41f0b1aa2f9469c6f
jsh <tmdgusya@naver.com>
2021-06-07 23:26:40 +0900

    feat: Login SingUp Routing

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
+import Login from '@pages/login';
+import signUp from '@pages/signUp';
 
 const App = () => {
-  return <div>Init</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={signUp} />
+    </Switch>
+  );
 };
 
 export default App;
---
+++ pages/signUp/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const signUp = () => {
+  return <div>회원가입</div>;
+};
+
+export default signUp;
